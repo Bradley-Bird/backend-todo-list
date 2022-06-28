0fb8ee7be139614f5c664e72e5887b159b55c10f
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-28 15:05:12 -0700

    test and sql written for todo table

---
+++ __tests__/todo.test.js
@@ -0,0 +1,43 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: '14@14.com',
+  password: '123123',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('todo routes', () => {
+  beforeEach(() => {
+    setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('Post to /api/v1/todos should create a new todo', async () => {
+    const [agent, user] = await registerAndLogin();
+    const resp = await agent.post('/api/v1/items').send({
+      todo: 'clean',
+      done: false,
+    });
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      user_id: user.id,
+      todo: 'clean',
+      done: false,
+    });
+  });
+});
