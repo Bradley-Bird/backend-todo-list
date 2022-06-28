fbdd2c5983926a8032caf8f8a1cc9f84315019f4
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-27 17:31:26 -0700

    first test passing, .post for users creates a new user.

---
+++ __tests__/users.test.js
@@ -0,0 +1,41 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+// const UserService = require('../lib/services/UserService');
+const mockUser = {
+  email: '14@14.com',
+  password: '123123',
+};
+
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
+
+//   // Create an "agent" that gives us the ability
+//   // to store cookies between requests in a test
+//   const agent = request.agent(app);
+
+//   // Create a user to sign in with
+//   const user = await UserService.create({ ...mockUser, ...userProps });
+
+//   // ...then sign in
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
+
+describe('users routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
+  });
+});
